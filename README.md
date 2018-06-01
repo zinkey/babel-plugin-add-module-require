c84301237f7452788101116564a5c490f5c0a477
zinkey <yaya@uloveit.com.cn>
2018-06-01 15:50:49 +0800

    babel-plugin-add-module-require

---
+++ README.md
@@ -0,0 +1,19 @@
+babel-plugin-add-module-exports
+
+---
+
+A plugin like https://github.com/59naga/babel-plugin-add-module-exports and support babel@7.
+
+Installation
+
+---
+
+```
+npm install babel-plugin-add-module-require --save-dev
+```
+
+Attention
+
+---
+
+This package is used for compatibility with previous projects using X. New projects are not recommended.
---
+++ index.js
@@ -0,0 +1,132 @@
+module.exports = function (babel) {
+  const { types: t } = babel;
+  return {
+    visitor: {
+      CallExpression(path) {
+        if (path.BABEL_PLUGIN_ADD_MODULE_REQUIRE || path.node.BABEL_PLUGIN_ADD_MODULE_REQUIRE || path.node.callee.name !== 'require' || !/^[\.\/]/.test(path.node.arguments[0].value)) {
+          return;
+        }
+        const requireExpression = t.callExpression(
+          t.identifier('require'),
+          path.node.arguments
+        );
+        const requireDefaultExpression = t.logicalExpression(
+          '||',
+          t.memberExpression(
+            requireExpression,
+            t.identifier('default'),
+            false
+          ),
+          requireExpression,
+        );
+        requireExpression.BABEL_PLUGIN_ADD_MODULE_REQUIRE = true;
+        path.replaceWith(
+          t.callExpression(
+            t.functionExpression(
+              null,
+              [],
+              t.blockStatement(
+                [
+                  t.variableDeclaration(
+                    'const',
+                    [
+                      t.variableDeclarator(
+                        t.identifier('obj'),
+                        requireDefaultExpression
+                      )
+                    ]
+                  ),
+                  t.ifStatement(
+                    t.identifier('obj'),
+                    t.expressionStatement(t.assignmentExpression(
+                      '=',
+                      t.memberExpression(
+                        t.identifier('obj'),
+                        t.stringLiteral('default'),
+                        true
+                      ),
+                      t.identifier('obj')
+                    )),
+                  ),
+                  t.returnStatement(
+                    t.identifier('obj')
+                  )
+                ]
+              )
+            ),
+            []
+          )
+        );
+        path.BABEL_PLUGIN_ADD_MODULE_REQUIRE = true;
+      },
+      ImportDeclaration(path) {
+        if (path.BABEL_PLUGIN_ADD_MODULE_REQUIRE || !/^[\.\/]/.test(path.node.source.value)) {
+          return;
+        }
+        const requireExpression = t.callExpression(t.identifier('require'), [path.node.source]);
+        const requireDefaultExpression = t.logicalExpression(
+          '||',
+          t.memberExpression(
+            requireExpression,
+            t.identifier('default'),
+            false
+          ),
+          t.objectExpression([]),
+        );
+        requireExpression.BABEL_PLUGIN_ADD_MODULE_REQUIRE = true;
+        const array = [];
+        const specifiers = path.node.specifiers.filter(item => {
+          if (item.type === 'ImportSpecifier') {
+            array.push(
+              t.variableDeclarator(
+                item.local,
+                t.conditionalExpression(
+                  t.binaryExpression(
+                    'in',
+                    item.imported,
+                    requireExpression
+                  ),
+                  t.memberExpression(
+                    requireExpression,
+                    item.imported,
+                    false
+                  ),
+                  t.memberExpression(
+                    requireDefaultExpression,
+                    item.imported,
+                    false
+                  ),
+                )
+              )
+            );
+            return false;
+          }
+          return true;
+        });
+        if (specifiers.length > 0) {
+          path.replaceWith(
+            t.importDeclaration(specifiers, path.node.source)
+          );
+          if (array.length > 0) {
+            path.insertAfter(
+              t.variableDeclaration(
+                'const',
+                array
+              )
+            );
+          }
+        } else {
+          if (array.length > 0) {
+            path.replaceWith(
+              t.variableDeclaration(
+                'const',
+                array
+              )
+            );
+          }
+        }
+        path.BABEL_PLUGIN_ADD_MODULE_REQUIRE = true;
+      }
+    }
+  };
+}
